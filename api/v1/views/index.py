4b374df5e5fc1e44f818efd6654ebab3c6e1a734
thebasitguy <mailbasitgh@gmail.com>
2024-05-29 19:02:41 -0700

    update

---
+++ api/v1/views/index.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3]
+"""
+API endpoints handler module.
+"""
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route("/status")
+def get_status():
+    """Returns the status of the API service."""
+    data = {"status": "OK"}
+    return jsonify(data)
+
+
+@app_views.route("/stats")
+def get_stats():
+    """Returns statistics about the API service."""
+    return jsonify(
+        {
+            "amenities": storage.count('Amenity'),
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "states": storage.count('State'),
+            "users": storage.count('User')
+        }
+    )
