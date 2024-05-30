4a8ee051f91cfee398c4b535bfe03db4cda90ce6
thebasitguy <mailbasitgh@gmail.com>
2024-05-29 19:07:20 -0700

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,30 +1,46 @@
-#!/usr/bin/python3]
+#!/usr/bin/python3
 """
-API endpoints handler module.
+index
 """
 
-from api.v1.views import app_views
 from flask import jsonify
+from api.v1.views import app_views
+
 from models import storage
 
 
-@app_views.route("/status")
-def get_status():
-    """Returns the status of the API service."""
-    data = {"status": "OK"}
-    return jsonify(data)
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    """
+    status route
+    :return: response with json
+    """
+    data = {
+        "status": "OK"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
 
 
-@app_views.route("/stats")
-def get_stats():
-    """Returns statistics about the API service."""
-    return jsonify(
-        {
-            "amenities": storage.count('Amenity'),
-            "cities": storage.count('City'),
-            "places": storage.count('Place'),
-            "reviews": storage.count('Review'),
-            "states": storage.count('State'),
-            "users": storage.count('User')
-        }
-    )
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
